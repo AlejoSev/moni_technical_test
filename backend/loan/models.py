74b157935d5c7cede5ebf61ecf7b72ed4b9c6809
Alejo <alejoseverini@gmail.com>
2025-04-08 16:08:13 -0300

    App started, LoanRequest model created

---
+++ backend/loan/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class LoanRequest(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+
+    dni = models.CharField(max_length=16)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    email = models.EmailField()
+    amount = models.DecimalField(max_digits=10, decimal_places=0)
+
+    def __str__(self):
+        return f"{self.name} ({self.dni})"
